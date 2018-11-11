60d132d277edc4ac0851f14e6863387d535d90cd
Alcedo Prieto <alcedoprieto@gmail.com>
2018-11-11 17:29:00 -0500

    Moviendo scripts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -117,7 +117,19 @@ Costo: <input id="idcosto" type="text" name="costo"><br>
                 }
             }, callback);
         }
+    var callback = function(response) {
+        tok = response.token;
+          if(!response.code){
+            console.log(response.token);
+          } else {
+            console.error('Error: ',response.error, 'Code: ', response.code, 'Message: ',response.message);
+          }
+        }
 
+    var kushki = new Kushki({
+        merchantId: "10000002310042414718149002935532", 
+        inTestEnvironment: true
+    });
 
     </script>
 @endsection
